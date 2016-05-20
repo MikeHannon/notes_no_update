f631f3a93b9f7f0f6b6b933102f6584c8f2cf340
Mike Hannon <mjhannon@gmail.com>
2016-05-19 17:05:57 -0700

    remove test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-class Fry(object):
-    """docstring for Fry"""
-    def __init__(self, arg):
-        super(Fry, self).__init__()
-        self.arg = arg
-        print self.arg
-
-
-fry = Fry("People")
